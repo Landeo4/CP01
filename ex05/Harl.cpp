5fdbd73411578abb1d91e8f43bd239d5914255af
Theo POTILLION <tpotilli@bess-f2r6s3.clusters.42paris.fr>
2024-06-03 16:04:27 +0200

    viral hit

---
+++ ex05/Harl.cpp
@@ -0,0 +1,18 @@
+#include "Harl.hpp"
+
+Harl::Harl(void)
+{
+	return ;
+}
+
+Harl::~Harl(void)
+{
+	return ;
+}
+
+void Harl::complain(std::string level)
+{
+
+}
+
+// utilise les pointeurs sur fonctions pas de if else if else
---
+++ ex05/Harl.hpp
@@ -0,0 +1,27 @@
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <string.h>
+#include <iostream>
+
+// class Harl{
+// 	public:
+
+// 	Harl(void);
+// 	void complain(std::string level){
+// 		static const std::map string, void(Harl::*)()const > m{
+// 			"debug", &Harl::debug.
+// 			"info", &Harl::info,
+// 			"warning" ,&Harl::warning,
+// 			"error", &Harl::error}
+// 		};
+// 	};
+// 	~Harl(void);
+// 	private:
+// 	void _debug(void);
+// 	void _info(void);
+// 	void _warning(void);
+// 	void error(void);
+// };
+
+#endif
